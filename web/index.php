ecc67baba8c4aea2018e3af504c37380e749cf01
fajar diki <fajarsiddiqi@gmail.com>
2020-03-24 08:23:53 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -37,12 +37,12 @@
 
 
 	// SELECT DATA 
-	$app->get('/bridgelog', function (Silex\Application $app, Request $request) {
+	$app->get('/bridgelog/{limit}', function($limit) use ($app) {
 		$result = array();
 
 		$time_start = microtime(true);
 
-		$sql = "SELECT * FROM bridge_log LIMIT 1000";
+		$sql = "SELECT * FROM bridge_log LIMIT $limit";
 		$stmt = $app['db']->query($sql);
 
 		while ($row=$stmt->fetch()) {
@@ -59,51 +59,30 @@
 		}
 
 		$time_end = microtime(true);
-
-        $execution_time = round(($time_end - $time_start), 3);
-
-        $result = number_format($execution_time,3);
+        $execution_time = $time_end - $time_start;
 
 		return $app->json(array('Bridge_Log' => $result, "Time" => $execution_time));
 	});
 
 	// SEARCH DATA
-	$app->get('/searchsilex/{msisdn}', function($msisdn) use ($app) {
-		$sql = "SELECT * FROM bridge_log WHERE msisdn='$msisdn'";
+	$app->get('/searchsilex/{cari}', function($cari) use ($app) {
+		$time_start = microtime(true);
+
+		$sql = "SELECT * FROM bridge_log WHERE msisdn='$cari'";
 		$stmt = $app['db']->query($sql);
 		$result = $stmt->fetchAll();
 
-		return $app->json(array('Bridge_Log' => $result));
+		$time_end = microtime(true);
+        $execution_time = $time_end - $time_start;
+
+		return $app->json(array('Bridge_Log' => $result, "Time" => $execution_time));
 	});
 
 	// UPDATE DATA
 	$app->post('/updatesilex', function(Silex\Application $app,Request $request) {
-		$id = $request->get('id');
+		$time_start = microtime(true);
 
-		$msisdn = $request->get('msisdn');
-		$called = $request->get('called');
-		$lat = $request->get('lat');
-		$lng = $request->get('lng');
-		$area = $request->get('area');
-		$ts = $request->get('ts');
-		$tenant = $request->get('tenant');
-
-
-		$sql = "UPDATE bridge_log SET msisdn='$msisdn', called='$called', lat='$lat', lng='$lat', area='$area', ts='$ts', tenant='$tenant' WHERE id='$id'";
-
-		$stmt = $app['db']->query($sql);
-
-		if ($stmt) {
-			return $app->json(array('message' => 'Update Succes'));
-		} else {
-			return $app->json(array('message' => 'Update Failed'));
-		}
-
-	});
-
-	// INSERT DATA
-	$app->post('/insertsilex', function(Silex\Application $app,Request $request) {
-
+		$jmlupdate = (int)$request->get('jumlahupdate');
 		$msisdn = $request->get('msisdn');
 		$called = $request->get('called');
 		$lat = $request->get('lat');
@@ -119,31 +98,95 @@
 			$id = $row['id'];
 		}
 
-		$lastid = $id+1;
+		$lastid = $id;
+		$maxulang = $lastid-$jmlupdate;
 
+		for ($i=$lastid; $i > $maxulang; $i--) {
+			$sql = "UPDATE bridge_log SET msisdn='$msisdn', called='$called', lat='$lat', lng='$lat', area='$area', ts='$ts', tenant='$tenant' WHERE id='$i'";
 
-		$sqlinsert = "INSERT INTO bridge_log VALUES ('$lastid', '$msisdn', '$called', '$lat', '$lng', '$area', '$ts', '$tenant')";
+			$stmt = $app['db']->query($sql);
+		}
 
-		$stmt = $app['db']->query($sqlinsert);
+		$time_end = microtime(true);
+		$execution_time = $time_end - $time_start;
 
 		if ($stmt) {
-			return $app->json(array('message' => 'Update Succes'));
+			return $app->json(array('message' => 'Update Succes', 'time' => $execution_time));
 		} else {
-			return $app->json(array('message' => 'Update Failed'));
+			return $app->json(array('message' => 'Update Failed', 'time' => $execution_time));
+		}
+
+	});
+
+	// INSERT DATA
+	$app->post('/insertsilex', function(Silex\Application $app,Request $request) {
+
+		$time_start = microtime(true);
+
+		$msisdn = $request->get('msisdn');
+		$called = $request->get('called');
+		$lat = $request->get('lat');
+		$lng = $request->get('lng');
+		$area = $request->get('area');
+		$ts = $request->get('ts');
+		$tenant = $request->get('tenant');
+		$jmlinput = (int)$request->get('jumlahinsert');
+
+		$sql = "SELECT MAX(id) as id FROM bridge_log";
+		$sqlid = $app['db']->query($sql);
+
+		while ($row=$sqlid->fetch()) {
+			$id = $row['id'];
+		}
+
+		$lastid = $id+1;
+		$maxulang = $lastid+$jmlinput;
+
+		for ($i=$lastid; $i < $maxulang; $i++) {
+			$sqlinsert = "INSERT INTO bridge_log VALUES ('$i', '$msisdn', '$called', '$lat', '$lng', '$area', '$ts', '$tenant')";
+			$stmt = $app['db']->query($sqlinsert);
+		}
+
+		$time_end = microtime(true);
+		$execution_time = $time_end - $time_start;
+
+		if ($stmt) {
+			return $app->json(array('message' => 'Update Succes', 'time' => $execution_time));
+		} else {
+			return $app->json(array('message' => 'Update Failed', 'time' => $execution_time));
 		}
 		// return $app->json(array('message' => $id, 'lastid' => $lastid));
 
 	});
 
 	// DELETE DATA
-	$app->get('/deletesilex/{id}', function($id) use ($app) {
-		$sql = "DELETE FROM bridge_log WHERE id='$id'";
-		$stmt = $app['db']->query($sql);
+	$app->get('/deletesilex/{jmldel}', function($jmldel) use ($app) {
+
+		$time_start = microtime(true);
+		$sql = "SELECT MAX(id) as id FROM bridge_log";
+		$sqlid = $app['db']->query($sql);
+
+		while ($row=$sqlid->fetch()) {
+			$id = $row['id'];
+		}
+		
+		$jml = (int)$jmldel;
+
+		$lastid = $id;
+		$maxulang = $lastid-$jml;
+
+		for ($i=$lastid; $i > $maxulang; $i--) {
+			$sql = "DELETE FROM bridge_log WHERE id='$i'";
+			$stmt = $app['db']->query($sql);
+		}
+
+		$time_end = microtime(true);
+		$execution_time = $time_end - $time_start;
 
 		if ($stmt) {
-			return $app->json(array('message' => 'Delete Succes'));
+			return $app->json(array('message' => 'Delete Succes', 'time' => $execution_time));
 		} else {
-			return $app->json(array('message' => 'Delete Failed'));
+			return $app->json(array('message' => 'Delete Failed', 'time' => $execution_time));
 		}
 	});
 
